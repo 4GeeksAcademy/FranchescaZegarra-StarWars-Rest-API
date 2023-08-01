519e8408a7cae9f11d3064826c1d2fdb075a106c
Franchesca Zegarra <129009072+Khrisefzm@users.noreply.github.com>
2023-08-01 03:10:00 +0000

    create User, Favorites, Vehicles and Planets

--- src/models.py
+++ src/models.py
@@ -2,18 +2,138 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship('Favorites', backref='user', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "favorites": list(map(lambda favorites: favorites.serialize(), self.favorites))
+            # do not serialize the password, its a security breach
+        }
+
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+    vehicles_id = db.Column(db.Integer, db.ForeignKey('vehicles.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "people_id": self.people_id,
+            "vehicles_id": self.vehicles_id,
+            "planets_id": self.planets_id,
+            # do not serialize the password, its a security breach
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    heigh = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(80))
+    skin_color = db.Column(db.String(80))
+    eye_color = db.Column(db.String(80))
+    birth_year = db.Column(db.String(80))
+    gender = db.Column(db.String(80))
+    favorites = db.relationship('Favorites', backref='people', lazy=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "heigh": self.heigh,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eyes_color": self.eyes_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "favorites": list(map(lambda favorites: favorites.serialize(), self.favorites))
+            # do not serialize the password, its a security breach
+        }
+
+class Vehicles(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    model = db.Column(db.String(80))
+    manufacturer = db.Column(db.String(80))
+    cost_in_credits = db.Column(db.Integer)
+    length = db.Column(db.Integer)
+    max_atmosphering_speed = db.Column(db.Integer)
+    crew = db.Column(db.String(80))
+    passengers = db.Column(db.Integer)
+    cargo_capacity = db.Column(db.Integer)
+    consumables = db.Column(db.String(80))
+    favorites = db.relationship('Favorites', backref='vehicles', lazy=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "manufacturer": self.manufacturer,
+            "cost_in_credits": self.cost_in_credits,
+            "length": self.length,
+            "max_atmosphering_speed": self.max_atmosphering_speed,
+            "crew": self.crew,
+            "passengers": self.passengers,
+            "cargo_capacity": self.cargo_capacity,
+            "consumables": self.consumables,
+            "favorites": list(map(lambda favorites: favorites.serialize(), self.favorites))
+            # do not serialize the password, its a security breach
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    rotation_period = db.Column(db.Integer)
+    orbital_period = db.Column(db.Integer)
+    diameter = db.Column(db.Integer)
+    climate = db.Column(db.String(80))
+    gravity = db.Column(db.String(80))
+    terrain = db.Column(db.String(80))
+    surface_water = db.Column(db.String(80))
+    population = db.Column(db.Integer)
+    favorites = db.relationship('Favorites', backref='planets', lazy=True)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "population": self.population,
+            "favorites": list(map(lambda favorites: favorites.serialize(), self.favorites))
             # do not serialize the password, its a security breach
         }
